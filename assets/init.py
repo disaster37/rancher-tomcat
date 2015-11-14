6cdb619ffd6a6c3ad49fcfc0be1b3f72e90b77ef
Sébastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2015-11-14 19:26:39 +0100

    wait init app before start tomcat

--- assets/init.py
+++ assets/init.py
@@ -104,7 +104,7 @@ class ServiceRun():
 
         <Channel className="org.apache.catalina.tribes.group.GroupChannel">
 
-            <Receiver className="org.apache.catalina.tribes.transport.nio.NioReceiver" autoBind="100" selectorTimeout="5000" maxThreads="6" address="''' + my_ip + '''" port="4444" />
+            <Receiver className="org.apache.catalina.tribes.transport.nio.NioReceiver" autoBind="9" selectorTimeout="5000" maxThreads="6" address="''' + my_ip + '''" port="4444" />
                 <Sender className="org.apache.catalina.tribes.transport.ReplicationTransmitter">
                     <Transport className="org.apache.catalina.tribes.transport.nio.PooledParallelSender" />
                 </Sender>
@@ -130,7 +130,7 @@ class ServiceRun():
 
     </Cluster>
     '''
-    self.replace_all(TOMCAT_PATH + '/conf/server.xml', re.escape('</Host>'), cluster_setting + "\n" + '</Host>')
+    self.replace_all(TOMCAT_PATH + '/conf/server.xml', re.escape('</Engine>'), cluster_setting + "\n" + '</Engine>')
 
     self.replace_all(TOMCAT_PATH + '/conf/context.xml', re.escape('</Context>'), '<Manager className="org.apache.catalina.ha.session.DeltaManager" expireSessionsOnShutdown="false" notifyListenersOnReplication="true" /></Context>')
 
